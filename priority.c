27d763cad6112b016b1fda2952bd44cea88af9b5
chandana123-glitch <chandanaot@gmail.com>
2026-03-11 09:24:28 +0530

    Update priority.c

--- priority.c
+++ priority.c
@@ -25,21 +25,57 @@ int main() {
         p[i].done = 0;
     }
 
-   ..
-     ..
-     ..
-     ..
-     ..
-     ..
-     ...
-     ...
+    int completed = 0;
+    int time = 0;
+
+    while (completed < n) {
+
+        int idx = -1;
+
+        for (int i = 0; i < n; i++) {
+
+            if (p[i].at <= time && p[i].done == 0) {
+
+                if (idx == -1)
+                    idx = i;
+
+                else if (p[i].pr < p[idx].pr)
+                    idx = i;
+
+                else if (p[i].pr == p[idx].pr && p[i].at < p[idx].at)
+                    idx = i;
+            }
+        }
+
+        if (idx != -1) {
+
+            time += p[idx].bt;
+
+            p[idx].ct = time;
+            p[idx].tat = p[idx].ct - p[idx].at;
+            p[idx].wt = p[idx].tat - p[idx].bt;
+
+            p[idx].done = 1;
+            completed++;
+        }
+        else {
+            time++;
+        }
+    }
+
+    float total_wt = 0, total_tat = 0;
+
     printf("Waiting Time:\n");
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
         printf("%s %d\n", p[i].pid, p[i].wt);
+        total_wt += p[i].wt;
+    }
 
     printf("Turnaround Time:\n");
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
         printf("%s %d\n", p[i].pid, p[i].tat);
+        total_tat += p[i].tat;
+    }
 
     printf("Average Waiting Time: %.2f\n", total_wt / n);
     printf("Average Turnaround Time: %.2f\n", total_tat / n);
